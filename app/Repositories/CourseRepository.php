cdc29536c94c4325280c7efbef8462696c1e0b6f
cjatoba <claytonjatoba@gmail.com>
2022-01-12 11:52:42 -0300

    03 - Cache: 03 - Trabalhando com Cache em APIs

--- app/Repositories/CourseRepository.php
+++ app/Repositories/CourseRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Course;
+use Illuminate\Support\Facades\Cache;
 
 class CourseRepository
 {
@@ -15,9 +16,22 @@ class CourseRepository
 
     public function getAllCourses()
     {
-        return $this->entity
-                    ->with('modules.lessons')
-                    ->get();
+        // Realiza a consulta no banco na primeira execução e guarda em cache por 60 segundos,
+        // nas demais requisições consulta o banco somente se a informação não existir no cache
+        // return Cache::remember('courses', 60, function () {
+        //     $this->entity
+        //                 ->with('modules.lessons')
+        //                 ->get();
+        // });
+
+        // Realiza a consulta no banco na primeira execução e guarda em cache,
+        // nas demais requisições consulta apenas o cache
+        return Cache::rememberForever('courses', function () {
+            return $this->entity
+                        ->with('modules.lessons')
+                        ->get();
+        });
+
     }
 
     public function createNewCourse(array $data)
